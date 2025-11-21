57a76cc06201be645eeaff0a22fdba72aacee13a
pedrocarvalho555 <pdmncarvalho@gmail.com>
2025-11-21 17:11:15 +0000

    Update ex_066.py

--- Exercicios/ex_066.py
+++ Exercicios/ex_066.py
@@ -22,10 +22,10 @@ print('--- SIMULADOR DE CRÉDITO ---')
 pedinte["Nome"] = input('Qual o seu nome?\n--> ')
 pedinte["AnoNasc"] = int(input('Em que ano nasceu?\n--> '))
 pedinte["Idade"] = 2025 - pedinte["AnoNasc"] #estou ciente que existe uma biblioteca para ir buscar o ano atual
-pedinte["Rendimento"] = int(input('Qual é o seu rendimento mensal?\n--> '))
-pedinte["Despesa"] = int(input('E quais são as suas despesas mensais?\n--> '))
+pedinte["Rendimento"] = float(input('Qual é o seu rendimento mensal?\n--> '))
+pedinte["Despesa"] = float(input('E quais são as suas despesas mensais?\n--> '))
 pedinte["RendaLiquida"] = pedinte["Rendimento"] - pedinte["Despesa"]
-pedinte["Credito"] = int(input('Qual é o valor do seu crédito?\n--> '))
+pedinte["Credito"] = float(input('Qual é o valor do seu crédito?\n--> '))
 pedinte["DuracaoCredito"] = int(input('Qual é o prazo do seu crédito em meses relembrando que cada ano tem 12 meses\n--> '))
 pedinte["PagamentoMensal"] = pedinte["Credito"] / pedinte["DuracaoCredito"]
 
@@ -34,14 +34,13 @@ if pedinte["PagamentoMensal"] < pedinte["RendaLiquida"]:
 else:
     pedinte["Aprovacao"] = 'Reprovado'
 
-print('A calcular o seu crédito.', end='')
-sleep(1)
-print('.', end='')
-sleep(1)
-print('.')
-sleep(2)
+print('A calcular o seu crédito', end='')
+for c in range (3):
+    sleep(1)
+    print('.', end='')
 
-print(f'Utilizador: {pedinte["Nome"]}')
+
+print(f'\nUtilizador: {pedinte["Nome"]}')
 print(f'Ano Nascimento: {pedinte["AnoNasc"]} - {pedinte["Idade"]} anos')
 print(f'Rendimento: {pedinte["Rendimento"]} | Despesa: {pedinte["Despesa"]} | Renda Liquida: {pedinte["RendaLiquida"]}')
 print(f'Credito: {pedinte["Credito"]} com duração de {pedinte["DuracaoCredito"]} meses')
